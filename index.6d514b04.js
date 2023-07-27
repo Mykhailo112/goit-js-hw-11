047104c0c643c85efed72844ceeaae047e2b64f4
Mykhailo112 <125221394+Mykhailo112@users.noreply.github.com>
2023-07-27 23:52:13 +0000

    Deploying to gh-pages from @ Mykhailo112/goit-js-hw-11@262e450ee49116e72ce3b3bb93a8d33d279225c1 🚀

---
+++ index.6d514b04.js
@@ -0,0 +1,2 @@
+SmoothScroll({animationTime:800,stepSize:75,accelerationDelta:30,accelerationMax:2,keyboardSupport:!0,arrowScroll:50,pulseAlgorithm:!0,pulseScale:4,pulseNormalize:1});
+//# sourceMappingURL=index.6d514b04.js.map
